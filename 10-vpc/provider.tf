b8f3b5cdf1b39e8c0531bbda7d972399465acc45
Amarnathg <amarnath.gotru4@gmail.com>
2025-01-03 23:18:02 +0530

    terraform

--- 10-vpc/provider.tf
+++ 10-vpc/provider.tf
@@ -1,10 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "5.66.0"
     }
   }
+
   backend "s3" {
     bucket         = "remote-state-81s"
     key            = "expense-vpcc"
@@ -12,6 +13,7 @@ terraform {
     dynamodb_table = "81s-locking"
   }
 }
+
 provider "aws" {
   # Configuration options
   region = "us-east-1"
