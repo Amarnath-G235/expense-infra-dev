b03cda1c3f771baecfd53abe6994e7155a001b76
Amarnathg <amarnath.gotru4@gmail.com>
2025-02-08 22:55:32 +0530

    terraform

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -1,17 +1,17 @@
 module "backend" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  ami  = data.aws_ami.joindevops.id
+  ami = data.aws_ami.joindevops.id
   name = local.resource_name
 
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.backend_sg_id]
   subnet_id              = local.private_subnet_id
 
-  tags = merge (
+  tags = merge(
     var.common_tags,
     var.backend_tags,
     {
-      Name = local.resource_name
+        Name = local.resource_name
     }
   )
 }
@@ -31,7 +31,7 @@ resource "null_resource" "backend" {
     password = "DevOps321"
   }
 
-    provisioner "file" {
+  provisioner "file" {
     source      = "${var.backend_tags.component}.sh"
     destination = "/tmp/backend.sh"
   }
@@ -43,12 +43,13 @@ resource "null_resource" "backend" {
       "sudo sh /tmp/backend.sh ${var.backend_tags.component} ${var.environment}"
     ]
   }
+
 }
 
 resource "aws_ec2_instance_state" "backend" {
   instance_id = module.backend.id
   state       = "stopped"
-  depends_on  = [null_resource.backend]
+  depends_on = [null_resource.backend]
 }
 
 resource "aws_ami_from_instance" "backend" {
@@ -63,11 +64,47 @@ resource "null_resource" "backend_delete" {
     instance_id = module.backend.id
   }
 
-
   provisioner "local-exec" {
     command = "aws ec2 terminate-instances --instance-ids ${module.backend.id}"
   }
 
   depends_on = [aws_ami_from_instance.backend]
-
 }
+
+resource "aws_lb_target_group" "backend" {
+  name     = local.resource_name
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 4
+  }
+}
+
+resource "aws_launch_template" "backend" {
+
+  name = local.resource_name
+  image_id = aws_ami_from_instance.backend.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+  
+  update_default_version = true
+  vpc_security_group_ids = [local.backend_sg_id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = local.resource_name
+    }
+  }
+}
+
